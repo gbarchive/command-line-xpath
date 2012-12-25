5b80940b43b40073d99c9e546dd0d9abca667643
Giuseppe Burtini <joe@truephp.com>
2012-12-25 11:30:09 -0800

    Basic autodetection.

--- xptest.rb
+++ xptest.rb
@@ -12,6 +12,8 @@ require 'rubygems'
 require 'nokogiri'	# XML/XPath parsing
 require 'open-uri'	# handles FTP/HTTP[S] URLs
 
+DEFAULT_MODE = :file
+
 def printVerbose(string) 
 	if ($options[:verbose] or $options[:extra_verbose]) then
 		$stderr.puts string
@@ -24,19 +26,27 @@ def printExtraVerbose(string)
 	end
 end
 
-def autodetectMode(args) 
-	# TODO: actually autodetect things.
-	:file
+def autodetectMode(fn) 
+	if File.exists?(fn) then
+		:file
+	elsif fn =~ URI::regexp
+		:url
+	else
+		DEFAULT_MODE
+	end
 end
 
 def getFile(fileName)
-	File.read(fileName)
+	File.read(fileName) if File.exists?(fileName)
 end
 
 def getURL(url)
-	# add default protocol here.
+	# TODO: add default protocol here.
+	# TODO: add support for user agent/referrer override here (via option)
+
 	result = ""	
 	open(url) do |data|
+		printVerbose("Read in " + data.base_uri.to_s + " (" + data.content_type.to_s + "). Last modified " + data.last_modified.to_s)
 		result = data.read
 	end
 	printExtraVerbose(result)
@@ -74,12 +84,20 @@ opts = OptionParser.new do |opts|
 end
 opts.parse!
 
-$options[:mode] = autodetectMode(ARGV) if $options[:mode] == :autodetect
 
 # TODO: detect which order they were passed in more intelligently
 #	to support user brainfarts... this is a convenience tool.
 input = ARGV[0]
 xpath_query = ARGV[1]
+
+$options[:mode] = autodetectMode(input) if $options[:mode] == :autodetect
+
+if (input.nil?) then
+	abort "Error: Please specify an input file."
+end
+if (xpath_query.nil?) then
+	abort "Error: Please specify a query."
+end
 
 printVerbose("Opening " + input + " for XPathing.")
 
@@ -91,6 +109,10 @@ case $options[:mode]
 	else
 		printVerbose("Error condition: :mode is unknown, value given was " + $options[:mode])
 		abort "I'm not sure what to do, exiting. (:mode is invalid)"
+end
+
+if data.nil? or data.empty? then
+	abort "Error: Specified " + $options[:mode].to_s + " had no content."
 end
 
 doc = Nokogiri::XML(data)
